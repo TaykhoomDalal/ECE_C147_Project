0b27de371bcfe94f60c27e7cbe843bafc021078c
kpstesla <kswhitecross@gmail.com>
2022-02-26 17:11:32 -0800

    bug fix

--- train_model.py
+++ train_model.py
@@ -46,7 +46,7 @@ def main():
                              pin_memory=args.pin_memory)
 
     # create model
-    model = models.__dict__()[args.model]
+    model = models.__dict__[args.model]
     if device is not None:
         model = model.to(device)
 
