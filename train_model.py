1ec5950badd9eabcd3ae3ff5e8193f97aad736f3
kpstesla <kswhitecross@gmail.com>
2022-03-02 09:29:36 -0800

    save

--- train_model.py
+++ train_model.py
@@ -57,7 +57,7 @@ def main():
                              pin_memory=args.pin_memory)
 
     # create model
-    model = models.__dict__[args.model](args.args)
+    model = models.__dict__[args.model](args.model_args)
     if device is not None:
         model = model.to(device)
 
