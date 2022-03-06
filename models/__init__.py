174dab8dcf696673a184f98ccb78d02ccd3f74e9
Taykhoom Dalal <tdalal@g.ucla.edu>
2022-03-05 17:07:39 -0800

    params for deepcnn

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,4 @@ from .RNN import SimpleRNN as simplernn
 from .RNN import SimpleLSTM as simplelstm
 from .CNN import SimpleCNN as simplecnn
 from .CNN import ShallowCNN as shallowcnn
+from .CNN import DeepCNN as deepcnn
