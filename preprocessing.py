8854f435af2a34c3ff7db09ad7f4d7972ff88a83
venmarco310 <veneng310@gmail.com>
2022-03-07 11:41:45 -0800

    Revert "updated CNN architecture"
    This reverts commit 953c15d8238564cfbbdb978e585c5d04179a1814.

--- preprocessing.py
+++ preprocessing.py
@@ -38,51 +38,12 @@ def normalize(x):
 def standardize(x):
     xStand = np.zeros_like(x)
     #Get Mean and StDev across all channels through trials and time bins
-    trainChannelMean = np.mean(x, axis=(-1, -3))
+    trainChannelMean = np.mean(X_train, axis=(-1, -3))
     trainChannelMean = trainChannelMean.reshape((22,1))
-    trainChannelStd = np.std(x, axis=(-1, -3))
+    trainChannelStd = np.std(X_train, axis=(-1, -3))
     trainChannelStd = trainChannelStd.reshape((22,1))
     # Use prevoius Values to standardize
     # Standardize across each trial
     for i in range(x.shape[0]):
         xStand[i] = (x[i] - trainChannelMean)/trainChannelStd
     return xStand
-
-def data_prep(X,y,sub_sample,average,noise):
-    
-    total_X = None
-    total_y = None
-    
-    # Trimming the data (sample,22,1000) -> (sample,22,500)
-    X = X[:,:,0:500]
-    print('Shape of X after trimming:',X.shape)
-    
-    # Maxpooling the data (sample,22,1000) -> (sample,22,500/sub_sample)
-    X_max = np.max(X.reshape(X.shape[0], X.shape[1], -1, sub_sample), axis=3)
-    
-    
-    total_X = X_max
-    total_y = y
-    print('Shape of X after maxpooling:',total_X.shape)
-    
-    # Averaging + noise 
-    X_average = np.mean(X.reshape(X.shape[0], X.shape[1], -1, average),axis=3)
-    X_average = X_average + np.random.normal(0.0, 0.5, X_average.shape)
-    
-    total_X = np.vstack((total_X, X_average))
-    total_y = np.hstack((total_y, y))
-    print('Shape of X after averaging+noise and concatenating:',total_X.shape)
-    
-    # Subsampling
-    
-    for i in range(sub_sample):
-        
-        X_subsample = X[:, :, i::sub_sample] + \
-                            (np.random.normal(0.0, 0.5, X[:, :,i::sub_sample].shape) if noise else 0.0)
-            
-        total_X = np.vstack((total_X, X_subsample))
-        total_y = np.hstack((total_y, y))
-        
-    
-    print('Shape of X after subsampling and concatenating:',total_X.shape)
-    return total_X,total_y
